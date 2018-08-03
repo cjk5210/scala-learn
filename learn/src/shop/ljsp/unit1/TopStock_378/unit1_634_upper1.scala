3452c74b3e23491516ef9d0c03ce53383d8599f1
c.jackong <332343786@qq.com>
2018-08-03 17:30:54 +0800

    init

---
+++ learn/src/shop/ljsp/unit1/TopStock_378/unit1_634_upper1.scala
@@ -0,0 +1,10 @@
+package shop.ljsp.unit1.TopStock_378
+
+class unit1_634_upper1 {
+  def upper(strings:String*):Seq[String]={
+    strings.map((s:String)=>s.toUpperCase())
+  }
+  val up=new unit1_634_upper1
+  println(up.upper("hello","World!"))
+}
+
